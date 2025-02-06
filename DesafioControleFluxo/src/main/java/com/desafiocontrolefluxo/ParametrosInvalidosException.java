1a5babeb3c77ef99d91afb366b8a051a11f375f8
matheuslima <mathlb.dev@hotmail.com>
2025-02-06 15:15:46 -0300

    Realizando desafio do curso da dio.

---
+++ DesafioControleFluxo/src/main/java/com/desafiocontrolefluxo/ParametrosInvalidosException.java
@@ -0,0 +1,7 @@
+package com.desafiocontrolefluxo;
+
+public class ParametrosInvalidosException extends Exception{
+    public ParametrosInvalidosException(String mensagem){
+        super(mensagem);
+    }
+}
